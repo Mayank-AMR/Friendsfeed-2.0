a448c3cacc0c6c8b98f3a861c2abea21d68ac455
Mayank Kumar <m1y1nk.amr@gmail.com>
2021-03-20 08:52:57 +0530

    Project initialised and add hilt & navigation dependency.

---
+++ app/src/main/java/com/mayank_amr/friendsfeed/MyApplication.kt
@@ -0,0 +1,14 @@
+package com.mayank_amr.friendsfeed
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * @Project Friendsfeed
+ * @Created_by Mayank Kumar on 19-03-2021 03:37 PM
+ */
+@HiltAndroidApp
+class MyApplication : Application() {
+
+    
+}
